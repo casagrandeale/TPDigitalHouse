ebd8d99268004460ee63435e91b8c0261e5ee110
Alejandro Casagrande <alecasagrande@advenio.com.ar>
2022-11-02 15:08:17 -0300

    agregados

--- TPFinal/airbnbCABATransformer.py
+++ TPFinal/airbnbCABATransformer.py
@@ -3,65 +3,66 @@ import numpy as np
 import pandas as pd
 import regex as re
 from datetime import datetime
+from sklearn.base import BaseEstimator, TransformerMixin
 
-def AirbnbCABATransformer(BaseEstimator, TransformerMixin):
+class AirbnbCABATransformer(BaseEstimator, TransformerMixin):
 
     def fit(self,X,y=None):
         return self
 
     def transform(self, X, y=None):
         data = X
-        data = _dropNotUsedColumns(data)
-        data = _processBathRooms(data)
-        data = _processBedRooms(data)
-        data = _convertToBoolean(data)
-        data = _processPrice(data)
-        data = _processPropertyType(data)
-        data = _processOutLiers(data)
-        data = _processHostDaysActive(data)
-        data = _processAmenities(data)
-        data = _processVerifications(data)
+
+        data = self._dropNotUsedColumns(data)
+        data = self._processBathRooms(data)
+        data = self._processBedRooms(data)
+        data = self._convertToBoolean(data)
+        data = self._processPrice(data)
+        data = self._processPropertyType(data)
+        data = self._processOutLiers(data)
+        data = self._processHostDaysActive(data)
+        data = self._processAmenities(data)
+        data = self._processVerifications(data)
                 
         data = data.fillna(0)
 
-        data = _processNeighbourhood(data)
-        data = _dropTextColumns(data)
-        data = _processGetdummies(data)
-        data = _convertToInt(data)
+        data = self._processNeighbourhood(data)
+        data = self._dropTextColumns(data)
+        data = self._processGetdummies(data)
+        data = self._convertToInt(data)
         
         return data
 
     def _dropNotUsedColumns(self,data):
-        data = data.drop(['id','source','availability_30','availability_60','availability_90','availability_365','listing_url','scrape_id','last_scraped','picture_url','host_id','host_url','host_name',
-       'host_location','host_neighbourhood','reviews_per_month','neighborhood_overview','neighbourhood','neighbourhood_group_cleansed',
-        'host_response_rate','host_thumbnail_url','host_about','host_response_time','host_response_rate',
-        'host_acceptance_rate','host_thumbnail_url','host_picture_url','host_listings_count',
-        'minimum_minimum_nights','maximum_minimum_nights','minimum_maximum_nights','maximum_maximum_nights',
-      'minimum_nights_avg_ntm','maximum_nights_avg_ntm','calendar_updated', 'calendar_last_scraped',
-      'has_availability','host_response_time','host_total_listings_count',
-      'number_of_reviews','number_of_reviews_ltm','number_of_reviews_l30d','first_review','last_review',
-      'review_scores_rating','review_scores_location','review_scores_accuracy','review_scores_cleanliness',
-                  'review_scores_checkin','review_scores_communication','review_scores_value',
-                  'license','calculated_host_listings_count','calculated_host_listings_count_entire_homes',
-        'calculated_host_listings_count_private_rooms','calculated_host_listings_count_shared_rooms'
-        ], axis=1)
+        
+        data = data.drop(['id'], axis=1)
+        data = data.drop(['source'], axis=1)
+        data = data.drop(['availability_30','availability_60','availability_90','availability_365','listing_url','scrape_id','last_scraped','picture_url','host_id','host_url','host_name'], axis=1)
+        data = data.drop(['host_location','host_neighbourhood','reviews_per_month','neighborhood_overview','neighbourhood','neighbourhood_group_cleansed'], axis=1)
+        data = data.drop(['host_response_rate','host_thumbnail_url','host_about','host_response_time','host_response_rate','host_acceptance_rate','host_thumbnail_url'], axis=1)
+        data = data.drop(['host_picture_url','host_listings_count', 'minimum_minimum_nights','maximum_minimum_nights','minimum_maximum_nights','maximum_maximum_nights'], axis=1)
+        data = data.drop(['minimum_nights_avg_ntm','maximum_nights_avg_ntm','calendar_updated', 'calendar_last_scraped','has_availability','host_total_listings_count'], axis=1)
+        data = data.drop(['number_of_reviews','number_of_reviews_ltm','number_of_reviews_l30d','first_review','last_review'], axis=1)
+        data = data.drop(['review_scores_rating','review_scores_location','review_scores_accuracy','review_scores_cleanliness'], axis=1)
+        data = data.drop(['review_scores_checkin','review_scores_communication','review_scores_value','license','calculated_host_listings_count','calculated_host_listings_count_entire_homes'], axis=1)
+        data = data.drop(['calculated_host_listings_count_private_rooms','calculated_host_listings_count_shared_rooms'], axis=1)
         return data
 
-    def _extractBathRoomType(row):  
+    def _extractBathRoomType(self,row):  
         if ("shared" in row["bathrooms_text"].lower()):
             return "shared"
         return "private"
 
-    def _extractBathQuantity(row):  
+    def _extractBathQuantity(self,row):  
         f = re.findall('\d*\.?\d+',row["bathrooms_text"])
         if (len(f)>0):
             return f[0]
         return "1"
     
-    def _processBathRooms(data):
+    def _processBathRooms(self,data):
         data["bathrooms_text"] = data["bathrooms_text"].astype(str)
-        data["bathroomtype"] = data.apply(lambda x:_extractBathRoomType(x),axis =1)   
-        data["bathrooms"] = data.apply(lambda x:_extractBathQuantity(x),axis =1)   
+        data["bathroomtype"] = data.apply(lambda x:self._extractBathRoomType(x),axis =1)   
+        data["bathrooms"] = data.apply(lambda x:self._extractBathQuantity(x),axis =1)   
 
         data["bathrooms"] = data["bathrooms"].astype(float)
         data["bathrooms"] = data["bathrooms"].apply(np.ceil)
@@ -75,22 +76,22 @@ def AirbnbCABATransformer(BaseEstimator, TransformerMixin):
     _patron = '((?P<numero>\d|mono|un|dos|tres|cuatro|cinco|seis|single|one|two|three|four|five|six|1|2|3|4|5|6)\s*((?P<ambiente>amb)|(?P<bedroom>hab|bedroom|bdr)))'
     _patron_regex = re.compile(_patron,flags = re.IGNORECASE)
 
-    def _extractBedRoom(row):
+    def _extractBedRoom(self,row):
         try:
             resultado=None
             ambientes=0
             if (row['description'] and isinstance(row['description'],str)):
-                resultado = _patron_regex.search(row['description'])
+                resultado = self._patron_regex.search(row['description'])
             if ((resultado is None) and row['name'] and isinstance(row['name'],str)):
-                resultado = _patron_regex.search(row['name'])
+                resultado = self._patron_regex.search(row['name'])
             if (resultado is not None):
                 qty = 1
-                if (resultado.group("numero") in _numbers_es):
-                    qty = _numbers_es.index(resultado.group("numero"))
+                if (resultado.group("numero") in self._numbers_es):
+                    qty = self._numbers_es.index(resultado.group("numero"))
                     if (qty == 0):
                         qty = 1
-                elif(resultado.group("numero") in _numbers_en):
-                    qty = _numbers_en.index(resultado.group("numero"))
+                elif(resultado.group("numero") in self._numbers_en):
+                    qty = self._numbers_en.index(resultado.group("numero"))
                     if (qty == 0):
                         qty = 1
                 elif(resultado.group("numero").isnumeric()):
@@ -104,33 +105,33 @@ def AirbnbCABATransformer(BaseEstimator, TransformerMixin):
             return np.nan
 
 
-    def _processBedRooms(data):
+    def _processBedRooms(self,data):
         #Relleno con patron regex según la palabra ambiente
-        data.loc[data.bedrooms.isnull(),'bedrooms'] = data.loc[data.bedrooms.isnull()].apply(_extractBedRoom,axis=1)
+        data.loc[data.bedrooms.isnull(),'bedrooms'] = data.loc[data.bedrooms.isnull()].apply(self._extractBedRoom,axis=1)
 
         data.loc[data.bedrooms.isnull(),'bedrooms'] = 1
         data.loc[data.beds.isnull(),'beds'] = 1
 
         return data
     
-    def _convertToBoolean(data):
+    def _convertToBoolean(self,data):
         data.replace({'f': 0, 't': 1}, inplace=True)
         return data
     
-    def _processPrice(data):
+    def _processPrice(self,data):
         data.price = data.price.str[1:-3]
         data.price = data.price.str.replace(",", "")
         data.price = data.price.astype('int64')
         return data
     
-    def _processPropertyType(data):
+    def _processPropertyType(self,data):
         data.loc[data["property_type"].str.contains("Entire cottage|Ranch|Shared room in guest suite|house|casa|villa|home|townhouse|chalet|Entire place|Entire cabin",case=False,na=False),"property_type"] = "House"
         data.loc[data["property_type"].str.contains("Cave|Shared room in ryokan|loft|apartment|dept|condo|floor|rental unit|Entire in-law|Entire guest suite|Private room in guest suite|Private room",case=False,na=False),"property_type"] = "Apartment"
         data.loc[data["property_type"].str.contains("Pension|hostel|hotel|bed and breakfast|resort",case=False,na=False),"property_type"] = "Hotel"
         data.loc[~data.property_type.isin(['House', 'Apartment','Hotel']), 'property_type'] = 'Other'
         return data
     
-    def _findOutliers(df, column, limit=4):
+    def _findOutliers(self,df, column, limit=4):
         """
         Devuelve outliers para una columna arriba del limite máximo
         Se puede ajustar el limite corriendo el 75%
@@ -146,21 +147,21 @@ def AirbnbCABATransformer(BaseEstimator, TransformerMixin):
             100 * (sum(outlier_mask) / len(df[column])),max_))
         return outlier_mask
 
-    def _processOutLiers(data):
+    def _processOutLiers(self,data):
         # quitar outliers de precios
-        data = data[np.logical_not(_findOutliers(data, 'price',limit=8))]
+        data = data[np.logical_not(self._findOutliers(data, 'price',limit=8))]
         # quitar outliers de baños
-        data = data[np.logical_not(_findOutliers(data, 'bathrooms',limit=6))]
+        data = data[np.logical_not(self._findOutliers(data, 'bathrooms',limit=6))]
 
         # quitamos outliers de camas
-        data = data[np.logical_not(_findOutliers(data, 'beds',limit=6))]
+        data = data[np.logical_not(self._findOutliers(data, 'beds',limit=6))]
 
         # quitamos outliers de noches minimas
-        data = data[np.logical_not(_findOutliers(data, 'minimum_nights',limit=14))]
+        data = data[np.logical_not(self._findOutliers(data, 'minimum_nights',limit=14))]
 
         return data
     
-    def _processHostDaysActive(data):
+    def _processHostDaysActive(self,data):
         #convertimos a dias la fecha desde que el host se encuentra activo
         data.host_since = pd.to_datetime(data.host_since) 
 
@@ -171,7 +172,7 @@ def AirbnbCABATransformer(BaseEstimator, TransformerMixin):
         data.host_days_active.fillna(data.host_days_active.median(), inplace=True)
         return data
 
-    def _processAmenities(data):
+    def _processAmenities(self,data):
         amenities = data.amenities.str.replace("[{}]", "").str.replace('"', "").str.replace('\\\\u2013', '-').str.replace('\\\\u2019','´').str.replace('\\\\u00f3','ó').str.replace('\\\\u00e9','é').str.replace('\\\\u00ed','í').str.replace('\\\\u00e1','á').str.replace('\\\\u00b4','').str.replace('\\\\u00f1','ñ').str.replace('\\\\u00a0','').str.replace('{', '').str.replace('}', ',').str.replace('[', '').str.replace(']', ',').str.replace('"', '')
         dict = [{"feature":"cable","values":["cable tv","TV with standard cable","cable"]}
        ,{"feature":"air_conditioning","values":["AC - split type ductless system","Air conditioning","aire acondicionado"]}
@@ -199,24 +200,24 @@ def AirbnbCABATransformer(BaseEstimator, TransformerMixin):
                 data.loc[data.amenities.str.contains(x,case=False,na=False),feat["feature"]] = 1 
         return data
 
-    def _processVerifications(data):
+    def _processVerifications(self,data):
         data.loc[data.host_verifications.str.contains('phone',case=False,na=False),'phone_verification'] = 1 
         data.loc[data.host_verifications.str.contains('work_email',case=False,na=False),'email_verification'] = 1 
         data.loc[data.host_verifications.str.contains('email',case=False,na=False),'email_verification'] = 1        
         return data
 
-    def _processNeighbourhood(data):
+    def _processNeighbourhood(self,data):
         data.loc[data.neighbourhood_cleansed == "Dique 1","neighbourhood_cleansed"] = "Puerto Madero"
         data.loc[data.neighbourhood_cleansed == "Dique 4","neighbourhood_cleansed"] = "Puerto Madero"
         data.loc[data.neighbourhood_cleansed == "Dique 3","neighbourhood_cleansed"] = "Puerto Madero"
         data.loc[data.neighbourhood_cleansed == "Dique 2","neighbourhood_cleansed"] = "Puerto Madero"
         return data
     
-    def _dropTextColumns(data):
+    def _dropTextColumns(self,data):
         data = data.drop(['name','description','amenities','host_since','host_verifications','latitude','longitude'], axis=1)
         return data
     
-    def _convertToInt(data):
+    def _convertToInt(self,data):
         data['bathrooms'] = data['bathrooms'].astype(int)
         data['bedrooms'] = data['bedrooms'].astype(int)
         data['host_days_active'] = data['host_days_active'].astype(int)
@@ -246,7 +247,7 @@ def AirbnbCABATransformer(BaseEstimator, TransformerMixin):
         data['email_verification'] = data['email_verification'].astype(int)
         return data
 
-    def _processGetdummies(data):
+    def _processGetdummies(self,data):
         data = pd.get_dummies(data)
         return data
 
